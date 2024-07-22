3f324c2c6ffc354e58f225b577fabb54ce55a1ac
s0u7a <kitan3409@outlook.com>
2024-07-22 23:43:28 +0900

    ss

--- main.tsx
+++ main.tsx
@@ -5,7 +5,7 @@ const app = new Hono()
 
 app.get('/', (c) => {
   let query = c.req.query('q')
-  let URL   = decodeURIComponent(atob(query));
+  let url   = decodeURIComponent(atob(query));
   return c.text(URL)
 })
 
